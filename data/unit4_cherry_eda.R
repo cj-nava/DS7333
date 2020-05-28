231bc7c50dad77bba9be9cbd27a7ac8842c2288b
jotsap <you@example.com>
2020-05-27 20:00:40 -0700

    Update on Cherry data cleaning

--- data/unit4_cherry_eda.R
+++ data/unit4_cherry_eda.R
@@ -8,6 +8,7 @@ library(RCurl)
 library(xml2)
 library(purrr)
 library(ggplot2)
+library(VIM)
 
 
 # women results
@@ -207,8 +208,212 @@ sapply(womenResMat, nrow)
 # formatting ages as numeric
 age <- map(womenResMat, ~ as.numeric(.x[ ,'ag']))
 
-# Look at womens box plots
+
+# we some have missing values
+sapply(age, function(x) sum(is.na(x)))
+
+
+# some of these are due to comments
+# can update the extractVariables function
+
+extractVariables = 
+  function(file, varNames =c("name", "home", "ag", "gun",
+                             "net", "time"))
+  {
+    
+    # Find the index of the row with =s
+    eqIndex = grep("^===", file)
+    # Extract the two key rows and the data 
+    spacerRow = file[eqIndex] 
+    headerRow = tolower(file[ eqIndex - 1 ])
+    body = file[ -(1 : eqIndex) ]
+    # Remove footnotes and blank rows
+    footnotes = grep("^[[:blank:]]*(\\*|\\#)", body)
+    if ( length(footnotes) > 0 ) body = body[ -footnotes ]
+    blanks = grep("^[[:blank:]]*$", body)
+    if (length(blanks) > 0 ) body = body[ -blanks ]
+    
+    
+    # Obtain the starting and ending positions of variables   
+    searchLocs = findColLocs(spacerRow)
+    locCols = selectCols(varNames, headerRow, searchLocs)
+    
+    Values = mapply(substr, list(body), start = locCols[1, ], 
+                    stop = locCols[2, ])
+    colnames(Values) = varNames
+    
+    return(Values)
+  }
+
+# update matrix
+womenResMat = lapply(womenFiles, extractVariables)
+
+# convert age to numeric
+age = sapply(womenResMat, function(x) as.numeric(x[ , 'ag']))
+
+# recheck missing values
+# results are better
+sapply(age, function(x) sum(is.na(x)))
+
+
+
+
+
+# we have some potential bad data
+# 2001 has a racer under age 7
+sapply(age, function(x) which(x < 7))
+
+# Look at womens box plots: filter out ages younger than 7
 age %>% enframe(name = "year", value = "age") %>% unnest() %>% filter(age, age > 7) %>% ggplot(aes(year, age)) + geom_boxplot() + ggtitle("Women's Ages 1999-2012")
+
+
+
+
+
+# NOTE: race times are not all in the same format
+charTime = womenResMat[['2012']][, 'time']
+head(charTime)
+tail(charTime)
+
+
+
+
+
+
+
+
+
+# Converting time by str_split, then mapping them to numeric.
+convertTime <- function(t) {
+  timePieces <- str_split(t, ":")
+  map_dbl(timePieces, function(x) {
+    x <- as.numeric(x)
+    if (length(x) == 2) 
+      x[1] + x[2]/60 
+    else 60 * x[1] + x[2] + x[3]/60
+  })
+}
+
+
+
+
+
+
+# Creating dataframe
+createDf = function(Res, year, sex) {
+  if (!is.na(Res[1, "net"])) 
+    useTime = Res[, "net"] else if (!is.na(Res[1, "gun"])) 
+      useTime = Res[, "gun"] else useTime = Res[, "time"]
+      
+      useTime = gsub("[#\\*[:blank:]]", "", useTime)
+      runTime = convertTime(useTime[useTime != ""])
+      
+      Res = Res[useTime != "", ]
+      age = gsub("X{2}\\s{1}?|\\s{3}?", "0  ", Res[, "ag"])
+      Res[, "ag"] = age
+      
+      Results = data.frame(year = rep(year, nrow(Res)), 
+                           sex = rep(sex, nrow(Res)), 
+                           name = Res[, "name"], 
+                           home = Res[, "home"], 
+                           age = as.numeric(Res[, "ag"]), 
+                           runTime = runTime, 
+                           stringsAsFactors = FALSE)
+      invisible(Results)
+}
+
+
+
+# gets every line that starts with ===
+separatorIdx <- grep("^===", womenFiles[["2006"]])
+
+# filters the list to 2006
+separatorRow <- womenFiles[["2006"]][separatorIdx]
+
+# makes a separator row
+paste(substring(separatorRow, 1, 63), " ", substring(separatorRow, 65, nchar(separatorRow)), sep = "") -> separatorRowX
+
+# replaces the === with the separator row
+womenFiles[["2006"]][separatorIdx] -> separatorRowX
+
+
+
+
+# extracts vars from the files
+womenResMat <- sapply(womenFiles, extractVariables)
+
+# makes a list of data frames from these things
+womenDF <- mapply(createDf, womenResMat, year = 1999:2012, sex = rep("W", 14), SIMPLIFY = FALSE)
+
+# investigage: HUGE DF w/ year hierarchy
+head(womenDF)
+
+# collapse into DF w/ year separated out from 1999 - 2012
+allWomen <- do.call(rbind, womenDF)
+
+
+
+
+
+# sorting by year, then run times
+# allWomen <- allWomen %>% dplyr::arrange(year, runTime)
+allWomen[order(allWomen$year, allWomen$runTime),] -> allWomen
+
+
+
+
+# we see 23 runners with an age of 0
+count(allWomen[allWomen$age == 0, ])
+
+# removing rows with 0 / NA's in age column
+allWomen <- allWomen[allWomen$age != 0, ]
+
+# summary stats for womens age
+allWomen %>% group_by(year) %>% summarise(ag_mean = mean(age, na.rm = T), 
+                                          ag_max = max(age, na.rm = T), 
+                                          ag_min = min(age, na.rm = T), 
+                                          ag_median = median(age, na.rm = T), 
+                                          ag_sd = sd(age, na.rm = T)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# show NA values
+
+# we can see 5432 missing values for runTime
+aggr(allWomen, 
+     prop = FALSE, 
+     combined = TRUE, 
+     numbers = TRUE, 
+     sortVars = TRUE, 
+     sortCombs = TRUE)
+
+# validate missing values
+sum(is.na(allWomen$runTime))
+is.na(allWomen$runTime)
+allWomen[is.na(allWomen$runTime),]
+
+# the NA's count as 0 showing the same 5432
+count(allWomen[allWomen$runTime == 0, ])
+
+
+
+
+
+
+
+
 
 
 
